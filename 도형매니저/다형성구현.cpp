7039fe86dfc1b86f3a8b1e519270aaa452e20521
KKH(15P-XD) <ke4145@gmail.com>
2023-12-28 15:56:27 +0900

    Practice 4. complete

--- 도형매니저/다형성구현.cpp
+++ 도형매니저/다형성구현.cpp
@@ -27,6 +27,11 @@ int main()
 	for (int i = 0; i < 10; ++i)
 		sm.insert(new Rectangle(Point(i, i + 1), Point(i * 2, i * 3)));
 
+	// 새 도형 추가
+	sm.insert(new Triangle(Point(0, 0), Point(1, 1), Point(2, 2)));
+	sm.insert(new Circle(Point(1.23, 4.56), 7.89));
+	sm.insert(new Rectangle(Point(1.23, 4.56), Point(1.23, 4.56)));
+
 	// 3. 관리하고 있는 모든 도형을 그림
 	sm.draw();
 
